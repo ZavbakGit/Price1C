3b2495bff1fd8f77decb14b1dad0a1c113b63c04
ZavbakGit <ag@799000.ru>
2019-08-01 22:21:33 +1000

    add viewmodel

--- app/src/main/java/fun/gladkikh/app/price1c/ui/fragment/MainFragment.kt
+++ app/src/main/java/fun/gladkikh/app/price1c/ui/fragment/MainFragment.kt
@@ -4,21 +4,21 @@ import `fun`.gladkikh.app.price1c.App
 import `fun`.gladkikh.app.price1c.R
 import `fun`.gladkikh.app.price1c.ui.activity.MainActivity
 import `fun`.gladkikh.app.price1c.ui.adapter.ItemPriceAdapter
-import `fun`.gladkikh.app.price1c.usecase.downLoadPrice
+import `fun`.gladkikh.app.price1c.viewmodel.PriseLisViewModel
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.uber.autodispose.AutoDispose
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.main_fragment.*
 import kotlinx.android.synthetic.main.swipe_list_item_price.*
 
 
 class MainFragment : BaseFragment() {
+
+    private lateinit var viewModel: PriseLisViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,16 +26,17 @@ class MainFragment : BaseFragment() {
             layoutManager = LinearLayoutManager(activity)
             adapter = ItemPriceAdapter()
         }
-
-
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this)
+            .get(PriseLisViewModel::class.java)
+    }
+
 
     override fun initSubscription() {
         super.initSubscription()
-        App.database.getItemPriceDao().getAllWithNameLike("","руб.").observe(this, Observer {
-            (activity as MainActivity).showMessage("Прочитали!")
-            (rvItemPrice.adapter as ItemPriceAdapter).updateData(it)
-        })
 
         edSearch.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
@@ -46,37 +47,36 @@ class MainFragment : BaseFragment() {
             }
 
             override fun onTextChanged(ch: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                changeText(ch.toString())
+                viewModel.changeSearchString(ch.toString())
+
             }
         })
 
-        swipeContainer.isRefreshing = false
+        viewModel.ldSearchString.observe(viewLifecycleOwner, Observer { searchString ->
+            App.database.getItemPriceDao().getAllWithNameLike("%$searchString%", "руб.")
+                .observe(this, Observer {
+                    (rvItemPrice.adapter as ItemPriceAdapter).updateData(it)
+                })
+        })
+
+
+        viewModel.ldShowProgress.observe(viewLifecycleOwner, Observer { show: Boolean ->
+            if (show) {
+                (activity as MainActivity).showProgress()
+            } else {
+                (activity as MainActivity).hideProgress()
+            }
+        })
+
+
         swipeContainer.setOnRefreshListener {
             swipeContainer.isRefreshing = false
-            (activity as MainActivity).showProgress()
-            downLoadPrice(activity as MainActivity)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doFinally {
-                    (activity as MainActivity).hideProgress()
-                }
-                .`as`(AutoDispose.autoDisposable(scopeProvider))
-                .subscribe({
-
-                }, {
-
-                })
-
+            viewModel.downloadPrice()
         }
 
+
     }
 
-    fun changeText(str:String){
-        App.database.getItemPriceDao().getAllWithNameLike("%$str%","руб.")
-            .observe(this, Observer {
-                (rvItemPrice.adapter as ItemPriceAdapter).updateData(it)
-            })
-    }
 
     override fun getLayout(): Int = R.layout.main_fragment
 }
